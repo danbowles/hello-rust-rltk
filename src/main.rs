4e2d031ad694125c0ccffcf8160e4a06c0281ef0
Dan Bowles <dbowles@alley.co>
2020-10-22 16:14:33 -0400

    rooms and corridors!

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,21 @@
-use rltk::{Rltk, GameState, Console, RGB, VirtualKeyCode};
+use rltk::{Rltk, GameState, RGB};
 use specs::prelude::*;
-use std::cmp::{max, min};
-use specs_derive::Component;
 
-#[derive(Component)]
-struct Position {
-  x: i32,
-  y: i32,
-}
+mod components;
+pub use components::*;
+mod map;
+pub use map::*;
+mod player;
+pub use player::*;
+mod rect;
+pub use rect::Rect;
 
-#[derive(Component)]
-struct Renderable {
-  glyph: u16,
-  fg: RGB,
-  bg: RGB,
-}
-
-#[derive(Component, Debug)]
-struct Player {}
-
-#[derive(Component)]
-struct LeftMover {}
-
-struct State {
+pub struct State {
   ecs: World,
 }
 
 impl State {
   fn run_system(&mut self) {
-    let mut lw = LeftWalker {};
-    lw.run_now(&self.ecs);
     self.ecs.maintain();
   }
 }
@@ -37,8 +23,12 @@ impl State {
 impl GameState for State {
   fn tick(&mut self, ctx: &mut Rltk) {
     ctx.cls();
-    self.run_system();
     player_input(self, ctx);
+    self.run_system();
+
+    let map = self.ecs.fetch::<Vec<TileType>>();
+    draw_map(&map, ctx);
+
     let positions = self.ecs.read_storage::<Position>();
     let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -48,49 +38,10 @@ impl GameState for State {
   }
 }
 
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-  type SystemData = (
-    ReadStorage<'a, LeftMover>,
-    WriteStorage<'a, Position>,
-  );
-
-  fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
-    for (_lefty, pos) in (&lefty, &mut pos).join() {
-      pos.x -= 1;
-      if pos.x < 0 { pos.x = 79; }
-    }
-  }
-}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-  let mut positions = ecs.write_storage::<Position>();
-  let mut players = ecs.write_storage::<Player>();
-
-  for (_player, pos) in (&mut players, &mut positions).join() {
-    pos.x = min(79, max(0, pos.x + delta_x));
-    pos.y = min(49, max(0, pos.y + delta_y));
-  }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-  // PLayer movement
-  match ctx.key {
-    None => {},
-    Some(key) => match key {
-      VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-      VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-      VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-      VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs,),
-      _ => {}
-    }
-  }
-}
-
 fn main() {
   use rltk::RltkBuilder;
   let context = RltkBuilder::simple80x50()
+    .with_tile_dimensions(12, 12)
     .with_title("Roguelike Tutorial")
     .build().unwrap();
   let mut gs = State {
@@ -99,12 +50,17 @@ fn main() {
 
   gs.ecs.register::<Position>();
   gs.ecs.register::<Renderable>();
-  gs.ecs.register::<LeftMover>();
   gs.ecs.register::<Player>();
+
+  let (map, rooms) = new_map_rooms_and_corridors();
+
+  gs.ecs.insert(map);
+
+  let (px, py) = rooms[0].center();
 
   gs.ecs
     .create_entity()
-    .with(Position { x: 40, y: 25 })
+    .with(Position { x: px, y: py })
     .with(Renderable {
       glyph: rltk::to_cp437('@'),
       fg: RGB::named(rltk::YELLOW),
@@ -112,19 +68,6 @@ fn main() {
     })
     .with(Player {})
     .build();
-  
-  for i in 0..10 {
-    gs.ecs
-    .create_entity()
-    .with(Position { x: i * 7, y: 20 })
-    .with(Renderable {
-        glyph: rltk::to_cp437('☺'),
-        fg: RGB::named(rltk::RED),
-        bg: RGB::named(rltk::BLACK),
-    })
-    .with(LeftMover {})
-    .build();
-  }
 
   rltk::main_loop(context, gs).unwrap();
 }
